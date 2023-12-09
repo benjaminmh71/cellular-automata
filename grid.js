ab3429ad78a6880209cd4507a9126f1af5de5d12
benjaminmh71 <benjaminmh71@gmail.com>
2023-12-08 22:56:30 -0500

    multiple states. final commit

--- grid.js
+++ grid.js
@@ -5,38 +5,23 @@ const speedButton1 = document.getElementById("speedButton1");
 const speedButton2 = document.getElementById("speedButton2");
 const speedButton3 = document.getElementById("speedButton3");
 const resetButton = document.getElementById("resetButton");
+const randomizeButton = document.getElementById("randomizeButton");
+const clearButton = document.getElementById("clearButton");
 const widthInput = document.getElementById("gridWidth");
 const heightInput = document.getElementById("gridHeight");
+const statesInput = document.getElementById("states");
 const offToOn = document.getElementById("offToOn");
 const onToOff = document.getElementById("onToOff");
+const ruleDiv = document.getElementById("ruleDiv");
 const cellSize = 3;
 var width = 0;
 var height = 0;
 var cells = [];
 var nextCells = [];
 var intervalID = 0;
+var states = 2;
+var ruleString;
 var rule;
-
-function load() {
-	cells = [];
-	rule = [[offToOn.value], [onToOff.value]];
-	width = Math.floor(widthInput.value / cellSize);
-	height = Math.floor(heightInput.value / cellSize);
-	canvas.setAttribute("width", widthInput.value - widthInput.value%3);
-	canvas.setAttribute("height", heightInput.value - heightInput.value%3);
-	for (let i = 0; i < width; i++) {
-		cells.push([]);
-		for (let j = 0; j < height; j++) {
-			cells[i].push(Math.floor(Math.random() * 2));
-		}
-	}
-	for (let i = 0; i < width; i++) {
-		nextCells.push([]);
-		for (let j = 0; j < height; j++) {
-			nextCells[i].push(0);
-		}
-	}
-}
 
 function getCell(x,y) { // Loops toroidally
 	if (x < 0) {
@@ -54,14 +39,14 @@ function getCell(x,y) { // Loops toroidally
 	return cells[x][y];
 }
 
-function neighborCount(x, y, target) {
+function neighborCount(x, y) {
 	let total = 0;
 	for (let i = -1; i <= 1; i++) {
 		for (let j = -1; j <= 1; j++) {
 			if (i == 0 && j == 0) {
 				continue;
 			}
-			if (getCell(x + i, y + j) == target) {
+			if (getCell(x + i, y + j) != 0) {
 				total++;
 			}
 		}
@@ -70,18 +55,11 @@ function neighborCount(x, y, target) {
 }
 
 function changeState(x, y, state) {
-	let rule0To1 = rule[0][0].split(",");
-	let rule1To0 = rule[1][0].split(",");
-	if (state == 0) {
-		for (let i = 0; i < rule0To1.length; i++) {
-			if (neighborCount(x, y, 1) == rule0To1[i]) {
-				return 1;
-			}
-		}
-	} else if (state == 1) {
-		for (let i = 0; i < rule1To0.length; i++) {
-			if (neighborCount(x, y, 1) == rule1To0[i]) {
-				return 0;
+	for (let i = 0; i < states; i++) {
+		if (i == state) { continue; }
+		for (let j = 0; j < rule[state][i].length; j++) {
+			if (neighborCount(x, y) == rule[state][i][j]) {
+				return i;
 			}
 		}
 	}
@@ -105,13 +83,9 @@ function update() {
 function draw() {
 	for (let i = 0; i < width; i++) {
 		for (let j = 0; j < height; j++) {
-			if (cells[i][j] == 1) {
-				ctx.fillStyle = "rgb(0,0,0)";
-				ctx.fillRect(i * 3, j * 3, 3, 3);
-			} else {
-				ctx.fillStyle = "rgb(255,255,255)";
-				ctx.fillRect(i * 3, j * 3, 3, 3);
-			}
+			let color = 256 - (256/(states-1))*cells[i][j];
+			ctx.fillStyle = "rgb(" + color + "," + color +"," + color + ")";
+			ctx.fillRect(i * 3, j * 3, 3, 3);
 		}
 	}
 }
@@ -125,16 +99,132 @@ function setSpeed(speed) {
 	}
 }
 
+function paintCells(event) {
+	const rect = canvas.getBoundingClientRect();
+	const x = Math.floor((event.clientX - rect.left) / cellSize);
+	const y = Math.floor((event.clientY - rect.top) / cellSize);
+	if (cells[x][y] == 0) {
+		cells[x][y] = 1;
+	} else {
+		cells[x][y] = 0;
+	}
+	draw();
+}
+
+function load() {
+	cells = [];
+	states = statesInput.value;
+	let ruleDivChildren = ruleDiv.children;
+	ruleString = [];
+	for (let i = 0; i < states; i++) {
+		ruleString.push([]);
+		for (let j = 0; j < states; j++) {
+			ruleString[i].push("");
+		}
+	}
+
+	for (let i = 0; i < ruleDivChildren.length; i++) {
+		let item = ruleDivChildren[i];
+		if (item.tagName == 'INPUT') {
+			ruleString[parseInt(item.id.charAt(0))][parseInt(item.id.charAt(3))] = item.value;
+		}
+	}
+	width = Math.floor(widthInput.value / cellSize);
+	height = Math.floor(heightInput.value / cellSize);
+	canvas.setAttribute("width", widthInput.value - widthInput.value % 3);
+	canvas.setAttribute("height", heightInput.value - heightInput.value % 3);
+
+	for (let i = 0; i < width; i++) {
+		cells.push([]);
+		for (let j = 0; j < height; j++) {
+			cells[i].push(Math.floor(Math.random() * 2));
+		}
+	}
+	for (let i = 0; i < width; i++) {
+		nextCells.push([]);
+		for (let j = 0; j < height; j++) {
+			nextCells[i].push(0);
+		}
+	}
+
+	rule = [];
+	for (let i = 0; i < states; i++) {
+		rule.push([]);
+		for (let j = 0; j < states; j++) {
+			rule[i].push([]);
+		}
+	}
+
+	for (let i = 0; i < states; i++) {
+		for (let j = 0; j < states; j++) {
+			let string = ruleString[i][j].split(",");
+			console.log(string);
+			if (string[0] == "") {
+				rule[i][j].push("-1");
+				continue;
+			}
+			for (let k = 0; k < string.length; k++) {
+				let list = string[k].split("-");
+				if (list.length > 1) {
+					for (let l = list[0]; l <= list[1]; l++) {
+						rule[i][j].push(l);
+					}
+				} else {
+					rule[i][j].push(list[0]);
+				}
+			}
+		}
+	}
+	console.log(rule);
+
+	draw();
+}
+
+function randomize() {
+	for (let i = 0; i < width; i++) {
+		for (let j = 0; j < height; j++) {
+			cells[i][j] = Math.floor(Math.random() * 2);
+		}
+	}
+	draw();
+}
+
+function clear() {
+	for (let i = 0; i < width; i++) {
+		for (let j = 0; j < height; j++) {
+			cells[i][j] = 0;
+		}
+	}
+	draw();
+}
+
+function updateStates() {
+	if (statesInput.value < 2) { return; }
+	let ruleDivString = "";
+	for (let i = 0; i < statesInput.value; i++) {
+		for (let j = 0; j < statesInput.value; j++) {
+			if (i == j) { continue; }
+			ruleDivString = ruleDivString +
+				"<label for=\"" + i + "To" + j + "\"> " + i + " to " + j + ":</label>" +
+				"<input type=\"text\" id=\"" + i + "To" + j + "\">";
+		}
+	}
+	ruleDiv.innerHTML = ruleDivString;
+}
+
 function run() {
 	update();
 	draw();
 }
 
 load();
-draw();
 intervalID = setInterval(run, 250); // Every 0.25 seconds
 pauseButton.onclick = function () { setSpeed(0); }
 speedButton1.onclick = function () { setSpeed(250); }
 speedButton2.onclick = function () { setSpeed(100); }
 speedButton3.onclick = function () { setSpeed(50); }
 resetButton.onclick = function () { load(); }
+randomizeButton.onclick = function () { randomize(); }
+clearButton.onclick = function () { clear(); }
+statesInput.onchange = function () { updateStates() }
+canvas.addEventListener("mousedown", function (e) { paintCells(e); })
